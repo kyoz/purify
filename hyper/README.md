419c6b6e8d75c2348279fcf5c3598042b7d7f4fa
Kyoz <banminkyoz@gmail.com>
2019-08-08 15:51:53 +0700

    init for hyper support

---
+++ hyper/README.md
@@ -0,0 +1,31 @@
+# Hyper
+> Instruction to apply purify colors for Hyper terminal
+
+<p align="center">
+  <img src="../demo/hyper.png" width="800px">
+</p>
+
+## Installation
+
+### Using command:
+
+```sh
+$ hyper install hyper-purify
+```
+
+### Using config file:
+
+Add `hyper-purify` to the plugins list in your `~/.hyper.js` config file and save it.
+
+```js
+plugins: [
+  "hyper-purify"
+]
+```
+
+## References
+
+If you are using zsh, please take a look at [purify/zsh](https://github.com/kyoz/purify/tree/master/zsh) to get zsh config for purify.
+
+## Lisence
+MIT © [Kyoz](mailto:banminkyoz@gmail.com)
---
+++ hyper/index.js
@@ -0,0 +1,56 @@
+'use strict'
+/*==============================================================================
+ Purify colors for Kitty
+ Author:     Kyoz
+ Repository: https://github.com/kyoz/purify/
+ License:    The MIT License (MIT)
+ Copyright:  Kyoz (banminkyoz@gmail.com)
+==============================================================================*/
+
+const foregroundColor = '#FAFAFA';
+const backgroundColor = '#313440';
+const selectionColor  = '#252834';
+
+const black           = '#282C34';
+const red             = '#FF6059';
+const green           = '#5FFF87';
+const yellow          = '#FFFF87';
+const blue            = '#5FAFFF';
+const magenta         = '#AF87FF';
+const cyan            = '#5FAFFF';
+const white           = '#FAFAFA';
+
+const lightBlack      = '#686868';
+
+exports.decorateConfig = config => Object.assign({}, config, {
+	backgroundColor,
+	foregroundColor,
+	borderColor: backgroundColor,
+	cursorColor: foregroundColor,
+	cursorAccentColor: backgroundColor,
+	selectionColor,
+	colors: {
+		black,
+		red,
+		green,
+		yellow,
+		blue,
+		magenta,
+		cyan,
+		white,
+		lightBlack,
+		lightRed: red,
+		lightGreen: green,
+		lightYellow: yellow,
+		lightBlue: blue,
+		lightMagenta: magenta,
+		lightCyan: cyan,
+		lightWhite: white
+	},
+	css: `
+		/* Hide title if there's just one tab */
+		.tabs_title {
+			display: none !important;
+		}
+	`
+});
